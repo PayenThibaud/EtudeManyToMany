f7dfcace1ead48b2f0d2d766e69dad1b0d970afa
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-03-01 14:28:39 +0100

    Préparation du projet, + ajout .gitignore

--- EtudeManyToMany/EtudeManyToMany.API/obj/Debug/net6.0/EtudeManyToMany.API.AssemblyInfo.cs
+++ EtudeManyToMany/EtudeManyToMany.API/obj/Debug/net6.0/EtudeManyToMany.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EtudeManyToMany.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6bb6822fc116ad76631ea64e6f15424552664e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EtudeManyToMany.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EtudeManyToMany.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EtudeManyToMany/EtudeManyToMany.Blazor/obj/Debug/net6.0/EtudeManyToMany.Blazor.AssemblyInfo.cs
+++ EtudeManyToMany/EtudeManyToMany.Blazor/obj/Debug/net6.0/EtudeManyToMany.Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EtudeManyToMany.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42535dcf7c159dad02d094ed9246fa2023ff9a64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6bb6822fc116ad76631ea64e6f15424552664e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EtudeManyToMany.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EtudeManyToMany.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
